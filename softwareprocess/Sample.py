d42b5cc4f2a5fbed9af04c61140e01c239c808e6
bkgunby <bkgunby@users.noreply.github.com>
2017-02-20 19:52:47 -0600

    Green Light |

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -74,7 +74,7 @@ class Sample(object):
             # print('old',simpsonOld)
             w = (highBound - lowBound) * 1.0 / s
             simpsonNew = f(lowBound, n) + f(highBound, n)
-            for i in range(1, int(s + 1)):
+            for i in range(1, int(s) + 1):
                 if i % 2 == 0:
                     simpsonNew += 2 * f(lowBound + i*w, n)
                 else:
