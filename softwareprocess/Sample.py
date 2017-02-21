714751cda0e8676044ff54fb89a661ad4ae9478a
bkgunby <bkgunby@users.noreply.github.com>
2017-02-20 18:13:31 -0600

    Red Light |

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -74,6 +74,7 @@ class Sample(object):
         s = 4.0
         while (abs((simpsonNew - simpsonOld) / simpsonNew) > epsilon):
             simpsonOld = simpsonNew
+            print('old',simpsonOld)
             w = (highBound - lowBound) * 1.0 / s
             simpsonNew = f(lowBound, n) + f(highBound, n)
             for i in range(1, int(highBound)):
