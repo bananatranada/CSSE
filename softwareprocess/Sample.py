b30e1b30b457e9c5cc8bdd23025bf8be46b4ef98
bkgunby <bkgunby@users.noreply.github.com>
2017-02-20 19:48:13 -0600

    Green Light |

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -74,7 +74,7 @@ class Sample(object):
         s = 4.0
         while (abs((simpsonNew - simpsonOld) / simpsonNew) > epsilon):
             simpsonOld = simpsonNew
-            print('old',simpsonOld)
+            # print('old',simpsonOld)
             w = (highBound - lowBound) * 1.0 / s
             simpsonNew = f(lowBound, n) + f(highBound, n)
             for i in range(1, int(s + 1)):
@@ -83,7 +83,7 @@ class Sample(object):
                 else:
                     simpsonNew += 4 * f(lowBound + i * w, n)
             simpsonNew *= (w / 3.0)
-            print('simpsonNew', simpsonNew)
+            # print('simpsonNew', simpsonNew)
             s *= 2
 
         return simpsonNew
