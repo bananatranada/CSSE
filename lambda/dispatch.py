95f0815281b12c4d6e78280eed1936355f5052a6
bkgunby <bkgunby@users.noreply.github.com>
2017-03-18 12:16:10 -0500

    Red Light |

--- lambda/dispatch.py
+++ lambda/dispatch.py
@@ -11,7 +11,7 @@ def dispatch(values=None):
 
     #Perform designated function
     if(values['op'] == 'adjust'):
-        return values    #<-------------- replace this with your implementation
+        return adjust(values)
     elif(values['op'] == 'predict'):
         return values    #This calculation is stubbed out
     elif(values['op'] == 'correct'):
@@ -21,3 +21,15 @@ def dispatch(values=None):
     else:
         values['error'] = 'op is not a legal operation'
         return values
+
+def adjust(values):
+    return values
+
+def predict(values):
+    return values
+
+def correct(values):
+    return values
+
+def locate(values):
+    return values
---
+++ lambda/test/NavigationTest.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+import dispatch as nav
+
+class NavigationTest(TestCase):
+    def shouldReturnErrorIfAltitudeExists(self):
+        input = {
+            'altitude': 'something'
+        }
+        expected = {
+            'altitude': 'something',
+            'error': 'altitude already exists in the input'
+        }
+        actual = nav.adjust(input)
+        self.assertDictEqual(actual, expected)
+
