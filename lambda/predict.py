936330c2588b78522c73dae5f521e50671435cbf
bkgunby <bkgunby@users.noreply.github.com>
2017-04-05 12:45:13 -0500

    Green Light |

--- lambda/predict.py
+++ lambda/predict.py
@@ -189,79 +189,11 @@ def predict(values):
 
     return output
 
-# def isLeapYear(year):
-#     if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-#         return True
-#     return False
-#
-# def numOfLeapYears(year1, year2):
-#     num = 0
-#     for year in range(year1, year2):
-#         if isLeapYear(year):
-#             num = num + 1
-#     return num
-#
-# def degreesFromFormattedAlt(f):
-#     degreesAndMinutes = f.split('d')
-#     degreesStr = degreesAndMinutes[0]
-#     degrees = int(degreesStr)
-#     # print('degrees', degrees)
-#     minutesStr = degreesAndMinutes[1]
-#     minutes = float(minutesStr)
-#     if degreesStr[0] == '-':
-#         return -1 * (abs(degrees) + arcminToDegrees(minutes))
-#     return degrees + arcminToDegrees(minutes)
-#
-#
-# def formatAlt(alt):
-#     print(alt)
-#     print(math.floor(alt))
-#     degrees = math.floor(alt)
-#     if alt < 0:
-#         degrees = math.ceil(alt)
-#     print('fa', degrees)
-#     arcmin = abs(round(degreesToArcmin(alt - degrees), 1))
-#     return '%dd%.1f' % (degrees, arcmin)
-#
-# def formatAndNormalizeAlt(alt):
-#     degrees = math.floor(alt)
-#     if alt < 0:
-#         degrees = math.ceil(alt)
-#     normalizedDegrees = degrees % 360
-#     print('fa', degrees)
-#     arcmin = abs(round(degreesToArcmin(alt - degrees), 1))
-#     return '%dd%.1f' % (normalizedDegrees, arcmin)
-#
-# def calcAltitude(totalDegrees, dip, refraction):
-#     return totalDegrees + dip + refraction
-#
-# def calcDip(height):
-#     return (-0.97 * math.sqrt(height)) / 60.0
-#
-# def calcRefraction(pressure, tempC, tangent):
-#     return (-0.00452*pressure) / ((273+tempC) * tangent)
-#
-# def convertToCelcius(f):
-#     return (f - 32) * 5.0/9.0
-#
-# def arcminToDegrees(min):
-#     return min / 60.0
-#
-# def degreesToArcmin(degrees):
-#     return degrees * 60.0
 
-
-
-def correct(values):
-    return values
-
-def locate(values):
-    return values
-
-input = {
-            'op': 'predict',
-            'body': 'Nunki',
-            'date': '2011-04-05'
-        }
-
-print(predict(input))
+# input = {
+#             'op': 'predict',
+#             'body': 'Nunki',
+#             'date': '2011-04-05'
+#         }
+#
+# print(predict(input))
