9e156774fc0928651426c091992b3d3e0e0415b9
bryan <bkang1308@users.noreply.github.com>
2017-04-27 21:54:13 -0500

    Green Light |

--- lambda/correct.py
+++ lambda/correct.py
@@ -280,7 +280,8 @@ def correct(values):
     # print(correctedDistance)
     # print(correctedAzimuth)
 
-    
+    output['correctedDistace'] = correctedDistance
+    output['correctedAzimuth'] = correctedAzimuth
 
     return output
 
